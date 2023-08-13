1631c2e89c3621847be2ac0ab485022f3dea2c2b
Roman Surkov <s.rb@list.ru>
2023-08-13 16:35:23 +0200

    Pageable method to get category products by id

--- src/main/java/com/surkoff/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/surkoff/ecommerce/dao/ProductRepository.java
@@ -1,9 +1,17 @@
 package com.surkoff.ecommerce.dao;
 
 import com.surkoff.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    /**
+     * Automatically expose http://localhost:8080/api/products/<b>search/findByCategoryId?id=2</b>
+     */
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
