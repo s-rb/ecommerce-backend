72be8401f054acb274a710f4f3eae2c16884cf30
Roman Surkov <s.rb@list.ru>
2023-09-07 20:43:33 +0200

    Added script for orders and addresses. Added entities

---
+++ src/main/java/com/surkoff/ecommerce/entity/Customer.java
@@ -0,0 +1,45 @@
+package com.surkoff.ecommerce.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "customer", schema = "ecommerce")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private Set<Order> orders = new HashSet<>();
+
+    public void add(Order order) {
+        if (order != null) {
+            if (orders == null) {
+                orders = new HashSet<>();
+            }
+
+            orders.add(order);
+            order.setCustomer(this);
+        }
+    }
+}
---
+++ src/main/resources/db/migration/V0006__orders_addresses.sql
@@ -0,0 +1,50 @@
+DROP TABLE IF EXISTS order_item cascade;
+DROP TABLE IF EXISTS orders cascade;
+DROP TABLE IF EXISTS customer cascade;
+DROP TABLE IF EXISTS address cascade;
+
+CREATE TABLE address (
+     id bigint NOT NULL,
+     city varchar(255),
+     country varchar(255),
+     state varchar(255),
+     street varchar(255),
+     zip_code varchar(255),
+     PRIMARY KEY (id)
+);
+
+CREATE TABLE customer (
+      id bigint NOT NULL,
+      first_name varchar(255),
+      last_name varchar(255),
+      email varchar(255),
+      PRIMARY KEY (id)
+);
+
+CREATE TABLE orders (
+        id BIGSERIAL NOT null primary key,
+        order_tracking_number varchar(255),
+        total_price decimal(19,2),
+        total_quantity int,
+        billing_address_id bigint UNIQUE,
+        customer_id bigint,
+        shipping_address_id bigint UNIQUE,
+        status varchar(128),
+        date_created timestamp,
+        last_updated timestamp,
+        CONSTRAINT FK_customer_id FOREIGN KEY (customer_id) REFERENCES customer (id),
+        CONSTRAINT FK_billing_address_id FOREIGN KEY (billing_address_id) REFERENCES address (id),
+        CONSTRAINT FK_shipping_address_id FOREIGN KEY (shipping_address_id) REFERENCES address (id)
+);
+
+CREATE TABLE order_item (
+        id bigint NOT NULL,
+        image_url varchar(255),
+        quantity int,
+        unit_price decimal(19,2),
+        order_id bigint,
+        product_id bigint,
+        PRIMARY KEY (id),
+        CONSTRAINT FK_order_id FOREIGN KEY (order_id) REFERENCES orders (id),
+        CONSTRAINT FK_product_id FOREIGN KEY (product_id) REFERENCES product (id)
+);
