3c04e6310c437516a22512f058c84e4a87a08ae0
Roman Surkov <s.rb@list.ru>
2023-10-04 20:24:23 +0200

    Send description to stripe

--- src/main/java/com/surkoff/ecommerce/service/CheckoutServiceImpl.java
+++ src/main/java/com/surkoff/ecommerce/service/CheckoutServiceImpl.java
@@ -77,6 +77,7 @@ public class CheckoutServiceImpl implements CheckoutService {
         params.put("amount", paymentInfo.getAmount());
         params.put("currency", paymentInfo.getCurrency());
         params.put("payment_method_types", paymentMethodTypes);
+        params.put("description", "My ecommerce description from backend");
 
         return PaymentIntent.create(params);
     }
